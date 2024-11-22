2a4d8331fa43f7d7cd3743c26bf87fcfe5695416
nischaltimalsina <nischaltimalcna@gmail.com>
2024-11-23 01:08:33 +0545

    fix: client side issye

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
+  entry: ['src/index.tsx'],
   external: ['react', 'next'],
   injectStyle: false,
+  sourcemap: false,
+  minify: true,
+  dts: true,
+  clean: true,
+  format: ['esm', 'cjs'],
+  splitting: false,
+  bundle: true
 })
